15e8b871cf7cdb48de950a937245ebe82ffd13ab
micahabanschick <micha.ban@gmail.com>
2021-04-05 17:57:26 -0400

    project and work reducers updates

--- v1/client/src/reducers/projectsReducer.js
+++ v1/client/src/reducers/projectsReducer.js
@@ -8,14 +8,17 @@ const initialState = {
 export default function projectsReducer(state = initialState, action) {
     switch (action.type) {
         case 'ADD_PROJECT':
-            let usersTwo = state.users.map(user => {
-              if (user.id === action.payload.id) {
-                return action.payload
-              } else {
-                return user
-              }
-            })
-            return {...state, ...action.payload.data, index: [...state.index, action.payload], user: usersTwo}
+          console.log(state)
+          // let project;
+          // if (state.userId === action.payload.data.id) {
+          //   project = action.payload
+          // } else {
+          //   project = {};
+          // }
+          console.log(action.payload)
+          // console.log(project)
+          let userId = action.payload.data.relationships.user.data.id
+          return Object.assign(state, { userId: userId }, { index: [...state.index, action.payload.data] })
         case 'EDIT_PROJECT':
             let usersThree = state.users.map(user => {
               if (user.id === action.payload.id) {
--- v1/client/src/reducers/worksReducer.js
+++ v1/client/src/reducers/worksReducer.js
@@ -8,14 +8,8 @@ const initialState = {
 export default function worksReducer(state = initialState, action) {
     switch (action.type) {
         case 'ADD_WORK':
-            let usersTwo = state.users.map(user => {
-              if (user.id === action.payload.id) {
-                return action.payload
-              } else {
-                return user
-              }
-            })
-            return {...state, ...action.payload.data, index: [...state.index, action.payload], user: usersTwo}
+          let userId = action.payload.data.relationships.user.data.id
+          return Object.assign(state, { userId: userId }, { index: [...state.index, action.payload] })
         case 'EDIT_WORK':
             let usersThree = state.users.map(user => {
               if (user.id === action.payload.id) {
