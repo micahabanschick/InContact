26529a47a6411284578a9f898c74d1fc61875491
micahabanschick <micha.ban@gmail.com>
2021-04-05 14:09:32 -0400

    add react-bootstrap: Container and Form in UserInput

--- v1/client/src/components/users/UserInput.js
+++ v1/client/src/components/users/UserInput.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 import {addUser} from '../../actions/users/addUser'
-import usersReducer from '../../reducers/usersReducer'
+// import usersReducer from '../../reducers/usersReducer'
 
 class UserInput extends Component {
   state = {
@@ -31,6 +34,11 @@ class UserInput extends Component {
       email: this.state.email
     })
   }
+
+  style = {
+    fontSize: '12', 
+    padding: '6em' 
+  }
   
   render() {
     console.log(this)
@@ -41,21 +49,35 @@ class UserInput extends Component {
     } else {
     // debugger
       return (
-        <div>
-        <h2>Inside User Input</h2>
-        <form onSubmit={this.handleSubmit}>
-        <label>Name: </label>
-        <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/>
-        <br/>
-        <label>Password: </label>
-        <input type='text' placeholder='Password' value={this.state.password} name="password" onChange={this.handleChange}/>
-        <br/>
-        <label>Email: </label>
-        <input type='text' placeholder='Email' value={this.state.email} name="email" onChange={this.handleChange}/>
-            <br/><br/>
-            <input type="submit"/>
-          </form>
-        </div>
+        <Container fluid="sm">
+          <h2>Inside User Input</h2>
+          <Form onSubmit={this.handleSubmit} style={this.style}>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Name" value={this.state.name} name="name" onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" value={this.state.password} name="password" onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter Email" value={this.state.email} name="email" onChange={this.handleChange} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
       )
     }
   }
