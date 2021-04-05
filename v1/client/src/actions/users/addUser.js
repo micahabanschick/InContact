d4547b9c9008560126ec1c67c64f1ef32999bc7b
micahabanschick <micha.ban@gmail.com>
2021-04-04 21:19:12 -0400

    add fetchUser

--- v1/client/src/actions/users/addUser.js
+++ v1/client/src/actions/users/addUser.js
@@ -1,3 +1,5 @@
+// import { browserHistory } from 'react-router-dom'
+
 export const addUser = (data) => {
 
   return (dispatch) => {
@@ -10,7 +12,10 @@ export const addUser = (data) => {
       body: JSON.stringify(data)
     })
     .then(response => response.json())
-    .then(user => dispatch({type: 'ADD_USER', payload: user}))
+    .then(user => {
+      dispatch({type: 'ADD_USER', payload: user});
+      // dispatch({type: 'FETCH_USER', payload: user});
+    })
   }
   
 }
