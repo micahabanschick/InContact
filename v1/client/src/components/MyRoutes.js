6623bf38242d466a4be27bfb9f2b3ec5a81ba3a0
micahabanschick <micha.ban@gmail.com>
2021-04-06 17:26:30 -0400

    User => Profile, WorksContainer => Resume, ProjectsContainer => Projects

--- v1/client/src/components/MyRoutes.js
+++ v1/client/src/components/MyRoutes.js
@@ -10,8 +10,9 @@ import Work from './works/Work'
 import WorkInput from './works/WorkInput'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
-import Resume from '../pages/Resume'
 import Home from '../pages/Home'
+import ProjectsContainer from '../containers/ProjectsContainer'
+import WorksContainer from '../containers/WorksContainer'
 
 const MyRoutes = (props) => {
 
@@ -21,21 +22,22 @@ const MyRoutes = (props) => {
 
   return (
     <Switch>
-      <Route exact path='/users/:id/home' render={(routerProps) => <Home {...routerProps} user={props.user}/>}/>
+      <Route exact path='/users/:id/home' render={(routerProps) => <Home {...routerProps} user={props.user} project={props.project} work={props.work}/>}/>
+      <Route exact path='/users/:id/profile' render={(routerProps) => <User {...routerProps} user={props.user} project={props.project} work={props.work}/>}/>
       <Route exact path='/users/:id/about' render={(routerProps) => <About {...routerProps} user={props.user}/>}/>
       <Route exact path='/users/:id/contact' render={(routerProps) => <Contact {...routerProps} user={props.user}/>}/>
-      <Route exact path='/users/:id/resume' render={(routerProps) => <Resume {...routerProps} user={props.user} work={props.work}/>}/>
-    
-      <Route exact path='/users/:userId/projects' render={(routerProps) => <Projects {...routerProps} project={props.project}/>}/>
-      <Route exact path='/users/:userId/projects/new' render={(routerProps) => <ProjectInput {...routerProps} project={props.project}/>}/>
-      <Route path='/users/:userId/projects/:id' render={(routerProps) => <Project {...routerProps} project={props.project}/>}/>
+      <Route exact path='/users/:id/resume' render={(routerProps) => <WorksContainer {...routerProps}/>}/>
+      <Route exact path='/users/:id/projects' render={(routerProps) => <ProjectsContainer {...routerProps}/>}/>
+    {
+      // <Route exact path='/users/:userId/projects/new' render={(routerProps) => <ProjectInput {...routerProps} project={props.project}/>}/>
+      // <Route path='/users/:userId/projects/:id' render={(routerProps) => <Project {...routerProps} project={props.project}/>}/>
       
-      <Route exact path='/users/:userId/works' render={(routerProps) => <Works {...routerProps} work={props.work}/>}/>
-      <Route exact path='/users/:userId/works/new' render={(routerProps) => <WorkInput {...routerProps} work={props.work}/>}/>
-      <Route path='/users/:userId/works/:id' render={(routerProps) => <Work {...routerProps} work={props.work}/>}/>
+      // <Route exact path='/users/:userId/works' render={(routerProps) => <Works {...routerProps} work={props.work}/>}/>
+      // <Route exact path='/users/:userId/works/new' render={(routerProps) => <WorkInput {...routerProps} work={props.work}/>}/>
+      // <Route path='/users/:userId/works/:id' render={(routerProps) => <Work {...routerProps} work={props.work}/>}/>
       
-      <Route exact path='/users' render={(routerProps) => <Users {...routerProps} user={props.user}/>}/>
-      <Route exact path='/users/:id' render={(routerProps) => <User {...routerProps} user={props.user}/>}/>
+      // <Route exact path='/users' render={(routerProps) => <Users {...routerProps} user={props.user}/>}/>
+    }
     </Switch> 
   )
 }
