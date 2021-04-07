38d638a0e59701559c6282bc6311ef9595dfb0dd
micahabanschick <micha.ban@gmail.com>
2021-04-06 22:19:44 -0400

    simplify and use correct variables with Projects

--- v1/client/src/components/projects/Projects.js
+++ v1/client/src/components/projects/Projects.js
@@ -12,19 +12,25 @@ class Projects extends Component {
     }
 
     render() {
-        return (
-            <div>
-                {
-                    this.props.projects && this.props.projects.map(project =>
-                        <li key={project.id}>
-                            <Link to={`/projects/${project.id}`}>{project.title} - ${project.description}</Link>
-                            <button onClick={() => this.handleDelete(project)}>Delete</button>
-                        </li> 
-                    )
-                }
-            </div>
-
-        )
+        console.log(this)
+        if (!this.props.project.index.id) {
+            return (
+                <h2>No Projects have been recorded.</h2>
+            )
+        } else {
+            return (
+                <div>
+                    {
+                        this.props.project.index.map(project =>
+                            <li key={project.id}>
+                                <Link to={`/projects/${project.id}`}>{project.title} - ${project.description}</Link>
+                                <button onClick={() => this.handleDelete(project)}>Delete</button>
+                            </li> 
+                        )
+                    }
+                </div>
+            )
+        }
     }
 }
 
--- v1/client/src/containers/ProjectsContainer.js
+++ v1/client/src/containers/ProjectsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ProjectInput from '../components/projects/ProjectInput'
-// import Projects from '../components/projects/Projects'
+import Projects from '../components/projects/Projects'
+import PortfolioProjects from '../pages/PortfolioProjects'
 
 class ProjectsContainer extends Component {
 
@@ -8,9 +9,10 @@ class ProjectsContainer extends Component {
   render() {
     return (
       <div>
-          <ProjectInput user={this.props.user}/><br/>
-          {
-            // <Projects projects={this.props.user && this.props.user.projects}/>
+        <PortfolioProjects project={this.props.project}/>
+        <ProjectInput project={this.props.project}/><br/>
+        <Projects project={this.props.project}/>
+      {
             console.log(this.props.match)
           }
       </div>
