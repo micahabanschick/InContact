089b00302a9399a80b7979d4a69a8f0784ca7756
micahabanschick <micha.ban@gmail.com>
2021-04-06 22:16:13 -0400

    add Accordion and Card

--- v1/client/src/components/projects/ProjectEdit.js
+++ v1/client/src/components/projects/ProjectEdit.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {editProject} from '../../actions/projects/editProject'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class ProjectEdit extends Component {
 
@@ -33,26 +36,39 @@ class ProjectEdit extends Component {
 
   render() {
     return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>Project Title: </label>
-                <input type='text' placeholder='Title' value={this.state.title} name="title" onChange={this.handleChange}/>
-                <br/>
-                <label>Project Description: </label>
-                <input type='text' placeholder='Description' value={this.state.description} name="description" onChange={this.handleChange}/>
-                <br/>
-                <label>Project Demo: </label>
-                <input type='text' placeholder='Demo' value={this.state.demo} name="demo" onChange={this.handleChange}/>
-                <br/>
-                <label>Project Tools: </label>
-                <input type='text' placeholder='Tools' value={this.state.tools} name="tools" onChange={this.handleChange}/>
-                <br/>
-                <label>Project Length: </label>
-                <input type='text' placeholder='Length' value={this.state.length} name="length" onChange={this.handleChange}/>
-                <br/><br/>
-                <input type="submit"/>
-            </form>
-        </div>
+      <div>
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Edit Project
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Project Title: </label>
+                    <input type='text' placeholder='Title' value={this.state.title} name="title" onChange={this.handleChange}/>
+                    <br/>
+                    <label>Project Description: </label>
+                    <input type='text' placeholder='Description' value={this.state.description} name="description" onChange={this.handleChange}/>
+                    <br/>
+                    <label>Project Demo: </label>
+                    <input type='text' placeholder='Demo' value={this.state.demo} name="demo" onChange={this.handleChange}/>
+                    <br/>
+                    <label>Project Tools: </label>
+                    <input type='text' placeholder='Tools' value={this.state.tools} name="tools" onChange={this.handleChange}/>
+                    <br/>
+                    <label>Project Length: </label>
+                    <input type='text' placeholder='Length' value={this.state.length} name="length" onChange={this.handleChange}/>
+                    <br/><br/>
+                    <input type="submit"/>
+                </form>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
     )
   }
 }
--- v1/client/src/components/works/WorkEdit.js
+++ v1/client/src/components/works/WorkEdit.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {editWork} from '../../actions/works/editWork'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class WorkEdit extends Component {
 
@@ -29,20 +32,38 @@ class WorkEdit extends Component {
 
   render() {
     return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-                <label>Work Title: </label>
-                <input type='text' placeholder='Title' value={this.state.title} name="title" onChange={this.handleChange}/>
-                <br/>
-                <label>Work Organization: </label>
-                <input type='text' placeholder='Organization' value={this.state.organization} name="organization" onChange={this.handleChange}/>
-                <br/>
-                <label>Work Length: </label>
-                <input type='text' placeholder='Length' value={this.state.length} name="length" onChange={this.handleChange}/>
-                <br/><br/>
-                <input type="submit"/>
-            </form>
-        </div>
+
+      <div>
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Edit Work
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Hello! I'm the body
+                <form onSubmit={this.handleSubmit}>
+                  <label>Work Title: </label>
+                  <input type='text' placeholder='Title' value={this.state.title} name="title" onChange={this.handleChange}/>
+                  <br/>
+                  <label>Work Organization: </label>
+                  <input type='text' placeholder='Organization' value={this.state.organization} name="organization" onChange={this.handleChange}/>
+                  <br/>
+                  <label>Work Length: </label>
+                  <input type='text' placeholder='Length' value={this.state.length} name="length" onChange={this.handleChange}/>
+                  <br/><br/>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </form>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+
+
+      </div>
     )
   }
 }
